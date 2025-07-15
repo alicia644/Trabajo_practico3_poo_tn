e65ccca230a04aaecbac4c261fbb6104471aa79e
alicia644 <142738434+alicia644@users.noreply.github.com>
2025-07-14 21:29:18 -0300

    Delete trabajo-practico3-alicia-cutac-poo-tn directory

--- trabajo-practico3-alicia-cutac-poo-tn/src/main/java/ar/org/centro8/java/models/enums/EstadoVenta.java
+++
@@ -1,6 +0,0 @@
-package ar.org.centro8.java.models.enums;
-
-public enum EstadoVenta {
-    PENDIENTE,PAGADO,CANCELADO;
-
-}
--- trabajo-practico3-alicia-cutac-poo-tn/src/main/java/ar/org/centro8/java/models/enums/Talle.java
+++
@@ -1,6 +0,0 @@
-package ar.org.centro8.java.models.enums;
-
-public enum Talle {
-    S,M,L,XL,XXL;
-
-}
